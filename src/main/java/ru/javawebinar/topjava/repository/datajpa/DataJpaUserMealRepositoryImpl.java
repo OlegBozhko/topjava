16d378e8e7575f0ac612bfa6c3f5863cdf681439
OlegBozhko <olehvasylkov@ukr.net>
2016-04-05 14:56:26 +0300

    HW5-do-not-work

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserMealRepositoryImpl.java
@@ -1,9 +1,15 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.repository.UserMealRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -12,29 +18,40 @@ import java.util.List;
  * 27.03.2015.
  */
 @Repository
+//@Transactional(readOnly = true)
 public class DataJpaUserMealRepositoryImpl implements UserMealRepository{
+    @PersistenceContext
+    private EntityManager em;
+
+    @Autowired
+    private ProxyUserMealRepository proxy;
+
     @Override
+    @Transactional
     public UserMeal save(UserMeal userMeal, int userId) {
-        return null;
+        User ref = em.getReference(User.class, userId);
+        userMeal.setUser(ref);
+        return proxy.save(userMeal);
     }
 
     @Override
+    @Transactional
     public boolean delete(int id, int userId) {
-        return false;
+        return proxy.delete(id, userId)!=0;
     }
 
     @Override
     public UserMeal get(int id, int userId) {
-        return null;
+        return proxy.findOne(id, userId);
     }
 
     @Override
     public List<UserMeal> getAll(int userId) {
-        return null;
+        return proxy.findAll(userId);
     }
 
     @Override
     public List<UserMeal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        return proxy.getBetween(startDate, endDate, userId);
     }
 }
